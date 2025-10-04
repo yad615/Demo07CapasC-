d56672a5247fc034e977f0188c9e9b10535394a2
htorrico <htorrico@truenorthcorporation.com>
2025-10-04 10:27:56 -0500

    Capa de datos

---
+++ Data/DProduct.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Microsoft.Data.SqlClient;
+
+namespace Data
+{
+    public class DProduct
+    {
+
+        public  string _connectionString = "Server=HUGO\\SQLEXPRESS01;Database=InvoicesCDB;" +
+            "Integrated Security=true; TrustServerCertificate=true";
+        public List<Product> Read()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand command = new SqlCommand("sp_ListarProductos", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product product = new Product
+                        {
+                            ProductId = Convert.ToInt32(reader["product_id"]),
+                            Name = reader["name"].ToString(),
+                            Price = Convert.ToDecimal(reader["price"]),
+                            Stock = Convert.ToInt32(reader["stock"]),
+                            Active = Convert.ToBoolean(reader["active"])
+                        };
+                        products.Add(product);
+                    }
+                }
+            }
+
+            return products;
+        }
+    }
+}
---
+++ Entity/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }   // product_id
+        public string Name { get; set; }     // name
+        public decimal Price { get; set; }   // price
+        public int Stock { get; set; }       // stock
+        public bool Active { get; set; }     // active
+    }
+}
